f19399a0208d6846608232e63078b488b07d627a
Moran <moranaga16@gmail.com>
2021-10-27 21:33:03 +0300

    Update NavBar.js

--- src/comopnents/navBar/NavBar.js
+++ src/comopnents/navBar/NavBar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./NavBar.css";
 import logo from "../../images/logo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const NavBar = () => {
   const catagories = [
-    "בגדי ים ",
-    "חזיות ",
-    "תחתונים ",
-    "הלבשת לילה ",
+    "בגדי ים  ",
+    "חזיות  ",
+    "תחתונים  ",
+    "הלבשת לילה  ",
     "מחטבים",
   ];
   return (
@@ -23,9 +26,9 @@ const NavBar = () => {
       </div>
 
       <div className="icons-container">
-        <i class="fas fa-search nav-icon"></i>
-        <i class="far fa-bookmark nav-icon"></i>
-        <i class="fas fa-shopping-cart nav-icon"></i>
+        <SearchIcon className="nav-icon" />
+        <BookmarkBorderIcon className="nav-icon" />
+        <ShoppingCartIcon className="nav-icon" />
       </div>
     </div>
   );
