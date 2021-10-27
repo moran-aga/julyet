c0498cae9706749077b9cf8cbbd930bd1e767b68
Moran <moranaga16@gmail.com>
2021-10-27 21:32:49 +0300

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ6866hulCG4Gv92kyD1Tee8ad8Pyssvk",
+  authDomain: "julyet-44363.firebaseapp.com",
+  projectId: "julyet-44363",
+  storageBucket: "julyet-44363.appspot.com",
+  messagingSenderId: "260699557689",
+  appId: "1:260699557689:web:6a8d3835370151b979a2e8",
+  measurementId: "G-RCJDCYF3R7",
+};
