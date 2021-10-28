b14ab1b0619fed92c896305982dcdf1c0e2e0de0
Moran <moranaga16@gmail.com>
2021-10-28 21:15:14 +0300

    update navbar

--- src/comopnents/navBar/NavBar.js
+++ src/comopnents/navBar/NavBar.js
@@ -4,31 +4,76 @@ import logo from "../../images/logo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const catagories = [
-    "בגדי ים  ",
     "חזיות  ",
     "תחתונים  ",
-    "הלבשת לילה  ",
-    "מחטבים",
+    "חליפות  ",
+    "מחטבים  ",
+    "בגדי ים  ",
+    "חלוקים ",
+    "פרוטזות שד ",
   ];
   return (
     <div className="navbar">
       <div className="logo-container">
-        <img src={logo} id="logo" alt="Julyet Logo" />
+        <Link to="/">
+          <img src={logo} id="logo" alt="Julyet Logo" />
+        </Link>
       </div>
 
-      <div className="catagories-container">
-        <span className="nav-catagory">
-          {catagories.map((catagory) => catagory)}
-        </span>
-      </div>
+      <ul className={"catagories-container"}>
+
+      <li className="nav-catagory">
+          <a href="/outlet" className="nav-catagory">
+            OUTLET
+          </a>
+        </li>
+
+        <li >
+          <a href="/breast-prostheses" className="nav-catagory">
+            פרוטזות שד
+          </a>
+        </li>
+        <li>
+          <a href="/sets" className="nav-catagory" className="nav-catagory">חליפות </a>
+        </li>
+        <li>
+          <a href="/swim-wear" className="nav-catagory">
+            בגדי ים
+          </a>
+        </li>
+
+        <li >
+          <a href="/robes" className="nav-catagory">
+            חלוקים
+          </a>
+        </li>
+        <li >
+          <a href="/night-wear" className="nav-catagory">
+            הלבשת לילה
+          </a>
+        </li>
+        <li >
+          <a href="/underwear" className="nav-catagory">
+            תחתונים
+          </a>
+        </li>
+        <li >
+          <a href="/bras" className="nav-catagory">
+            חזיות
+          </a>
+        </li>
+      </ul>
 
       <div className="icons-container">
         <SearchIcon className="nav-icon" />
         <BookmarkBorderIcon className="nav-icon" />
-        <ShoppingCartIcon className="nav-icon" />
+        <Link to="/checkout">
+          <ShoppingCartIcon className="nav-icon" />
+        </Link>
       </div>
     </div>
   );
